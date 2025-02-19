ab7137e8b58b4a294471486355fefba9838951b3
DennisRuizB <127039219+DennisRuizB@users.noreply.github.com>
2025-02-19 20:08:05 +0100

    Ejercicios entrega seminario JS

--- 5-SeminarioJS/ejercicios.js
+++ 5-SeminarioJS/ejercicios.js
@@ -25,65 +25,79 @@ function getUser(userId) {
       });
   }
   
-  // Encadenando Promesas
-  console.log("Inicio");
-  
-  
-  getUser(1)
-    .then((user) => getPosts(user.id))
-    .then((posts) => getComments(posts[0].id))
-    .then(comments => {
-      console.log("Comentarios del primer post:", comments);
-      console.log("Fin");
-    })
-    .catch(error => console.error("Error:", error));
+
 
 //ejercicio 1: JSON de todos los parametros de un user
 getUser(2)
-  .then((result)=>console.log(result))
+  .then((result)=>{
+    console.log("Ejercicio 1: ")
+    console.log(result)
+  })
   .catch(error => console.error("Error:", error));
+
+
+
 
 //ejercicio 2: Pedir los posts de un usuario
 getUser(2)
  .then((user) => getPosts(user.id))
- .then(result=> console.log(result))
- .catch(error => console.error("Error:", error));
+ .then(result=> {
+  console.log("Ejercicio 2:")
+  console.log(result)})
+ .catch(error => console.error("Error:", error)); 
+
+ 
 
 //ejercicio 3: pedir los comentarios de los posts
  getUser(2)
  .then(user => getPosts(user.id)) // Obtiene los posts del usuario
  .then(posts => Promise.all(posts.map(post => getComments(post.id)))) // Obtiene los comentarios de cada post en paralelo (gracias a Promise.all)
  .then(commentsArray => {
+   console.log("Ejercicio 3: con promesas")
    commentsArray.forEach((comments, index) => {
      console.log(`Comentarios del post ${index}:`, comments);
    });
  })
  .catch(error => console.error("Error:", error));
 
+ //usando async
+ async function fetchOrderPostComments() {
+  try {
+    const user = await getUser(2);
+    const posts = await getPosts(user.id);
+    let comments = await Promise.all(posts.map(post => getComments(post.id))); // Obtiene los comentarios de cada post en paralelo (gracias a Promise.all)
+    console.log("Ejercicio 3: con async")
+    comments.forEach((comments, index) => {
+        console.log(`Comentarios del post ${index}:`, comments);
+      });
+    console.log("Fin");
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+console.log("Inicio");
+
+fetchOrderPostComments();
+
+
+
  //ejercicio 4: Usar 3 de las funciones de alto nivel combinadas (map, reduce, filte,...) con los datos. 
 
 
  getUser(2)
  .then((user) => getPosts(user.id))
- .then(posts => console.log(posts
+ .then(posts => {
+  console.log("Ejercicio 4: ")
+  console.log(posts
   .filter((post) => post.id < 18)
   .sort((a,b) => b.id - a.id)
   .map((posts) => ({
   ...posts,
-  initialDate: `${new Date().toLocaleDateString('es-ES')}`}))))
-
+  initialDate: `${new Date().toLocaleDateString('es-ES')}`})))})
  .catch(error => console.error("Error:", error))
 
 
 
 
- //REFERENCES 
- /*
-  - Videos recomenats de JS a atenea
-  - ChatGPT para Promise.all
- */
-
-
  
 
-  
